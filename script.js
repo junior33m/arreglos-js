f5da265bdfc317ca0d38d997466f6a55351dacb9
junior <juniormercado059@gmail.com>
2025-07-24 20:09:56 -0500

    mi remoto

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const contenedor= document.getElementById("elemento")
+const input= document.getElementById("textinput")
+
+let elementos= []
+
+function AgregarElemento(){
+    if( input.value != ""){
+        elementos.push( input.value)
+
+        let resultado = ""
+        for (let i = 0; i < elementos.length; i++) {
+           resultado += " <span>" + elementos[i] + "</span>"
+        }
+        contenedor.innerHTML= resultado
+
+    }
+
+}
